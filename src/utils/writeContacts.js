0437a8edf3dd076cafb5fb2c3ba35cf5c3eab2c7
SychTim <103521623+SychTim@users.noreply.github.com>
2024-11-04 21:50:11 +0100

    finished job

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
+import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) =>
+  writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
